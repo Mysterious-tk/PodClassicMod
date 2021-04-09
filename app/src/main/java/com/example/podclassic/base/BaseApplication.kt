e1e1dba345792b401564674ea28d3e4871ef4e57
0x1317bf7 <823222063@qq.com>
2021-04-09 22:46:58 +0800

    _

--- app/src/main/java/com/example/podclassic/base/BaseApplication.kt
+++ app/src/main/java/com/example/podclassic/base/BaseApplication.kt
@@ -3,12 +3,7 @@ package com.example.podclassic.base
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
-import android.os.Environment
 import com.example.podclassic.`object`.Core
-import java.io.File
-import java.io.FileWriter
-import java.text.SimpleDateFormat
-import java.util.*
 
 class BaseApplication : Application() {
     companion object {
@@ -20,6 +15,7 @@ class BaseApplication : Application() {
         super.onCreate()
         context = this
         Core.init()
+        /*
 
         Thread.setDefaultUncaughtExceptionHandler { _, e ->
             val file = File(Environment.getExternalStorageDirectory().path + File.separator + "log.txt");
@@ -33,5 +29,7 @@ class BaseApplication : Application() {
             }
             fw.close()
         }
+
+         */
     }
 }
